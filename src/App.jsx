ed995445ceedda193b98bc8ab5970250781a0817
DoCodingkeval <gohelkeval7@gmail.com>
2025-06-14 17:26:25 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
-import Hero from './components/Hero'
-import Nav from './components/Nav'
-import Modeservice from './components/Modeservice'
-import Service from './components/Service'
-import Showcase from './components/Showcase'
-import Testimonial from './components/Testimonial'
-import Tips from './components/Tips'
-import { RiArrowUpLine } from 'react-icons/ri'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import GotoTop from './GotoTop'
+import { lazy, Suspense, useEffect, useState } from "react";
+import { RingLoader } from "react-spinners";
+const Hero = lazy(() => import("./components/Hero"));
+const Nav = lazy(() => import("./components/Nav"));
+const Modeservice = lazy(() => import("./components/Modeservice"));
+const Service = lazy(() => import("./components/Service"));
+const Showcase = lazy(() => import("./components/Showcase"));
+const Testimonial = lazy(() => import("./components/Testimonial"));
+const Tips = lazy(() => import("./components/Tips"));
+const Contact = lazy(() => import("./components/Contact"));
+const Footer = lazy(() => import("./components/Footer"));
+const GotoTop = lazy(() => import("./GotoTop"));
 
 function App() {
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const Timer = setTimeout(() => {
+      setLoading(true);
+    }, 2500);
+    return () => clearTimeout(Timer);
+  }, []);
+
+  const LoadingUI = (
+    <div className="d-flex flex-column align-items-center justify-content-center h-100">
+      <RingLoader color="blue" size={100} />
+      <h3 className="pt-4">Just Hang on, it's cooking something...</h3>
+    </div>
+  );
+
   return (
-    <main>
-      <Nav />
-      <Hero />
-      <Service />
-      <Showcase />
-      <Modeservice />
-      <Testimonial />
-      <Tips />
-      <Contact />
-      <GotoTop />
-      <Footer />
-    </main>
-  )
+    <>
+      {isLoading ? (
+        <main>
+          <Suspense fallback={<div>Error...</div>}>
+            <Nav />
+            <Hero />
+            <Service />
+            <Showcase />
+            <Modeservice />
+            <Testimonial />
+            <Tips />
+            <Contact />
+            <GotoTop />
+            <Footer />
+          </Suspense>
+        </main>
+      ) : (
+        LoadingUI
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
